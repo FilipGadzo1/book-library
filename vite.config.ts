c234e8e5895add557dd79c3995eb12e729d10025
Filip Gadzo <filip@spaceinch.com>
2024-04-18 11:12:52 +0200

    add new congfig

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from 'unplugin-vue-components/resolvers';
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  base: '/book-library/',
   plugins: [
     vue(),
     vueJsx(),
